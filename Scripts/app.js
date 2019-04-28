97b0def7a6eb13a13f3c3c4d8e3a5b7c7648b053
Javid <j-n_01@hotmail.com>
2019-04-28 16:19:24 -0400

    Initial Commit

---
+++ Scripts/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+
+        var height = $('.navbar').height();
+        var scrollTop = $(window).scrollTop();
+
+        if (scrollTop >= height - 40) {
+            $('.nav-container').addClass('solid-nav');
+        } else {
+            $('.nav-container').removeClass('solid-nav');
+        }
+
+    });
+});
